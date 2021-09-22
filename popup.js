e3b6eed34515b0a3069e03c6a8801732d81e47e3
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-09-22 11:24:29 -0400

    update app description and add preview of background text color in the popup box text

--- popup.js
+++ popup.js
@@ -24,26 +24,36 @@ function run(tab) {
 
 function yellowClick() {
   document.getElementById('buttonClicked').style.background = "#ffffcc";
+  document.getElementById('header-1').style.background = "#ffffcc";
+  document.getElementById('header-2').style.background = "#ffffcc";
   colourChoice = "yellow";
 }
 
 function blueClick() {
   document.getElementById('buttonClicked').style.background = "#b3d9ff";
+  document.getElementById('header-1').style.background = "#b3d9ff";
+  document.getElementById('header-2').style.background = "#b3d9ff";
   colourChoice = "blue";
 }
 
 function redClick() {
   document.getElementById('buttonClicked').style.background = "#ffe368";
+  document.getElementById('header-1').style.background = "#ffe368";
+  document.getElementById('header-2').style.background = "#ffe368";
   colourChoice = "red";
 }
 
 function greenClick() {
   document.getElementById('buttonClicked').style.background = "#75baff";
+  document.getElementById('header-1').style.background = "#75baff";
+  document.getElementById('header-2').style.background = "#75baff";
   colourChoice = "green";
 }
 
 function pinkClick() {
   document.getElementById('buttonClicked').style.background = "#b3b3b3";
+  document.getElementById('header-1').style.background = "#b3b3b3";
+  document.getElementById('header-2').style.background = "#b3b3b3";
   colourChoice = "pink";
 }
 
